cba7759ecdd6774b55dec6183f7641c992560d4d
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2021-11-17 08:04:47 +0530

    ADDED : DIAGONAL MATRIX

---
+++ diagonalMatrix.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Matrix{
+    private:
+        int* array;
+        int size;
+    public:
+        Matrix(int size){
+            this->size = size;
+            this->array= new int[size];
+            this->initialize();
+        }
+        void initialize(){
+            for (int i = 0; i < this->size; i++){
+                this->array[i]=0;
+            }
+            
+        }
+        void set(int data,int i,int j){
+            if(i==j){
+                this->array[i] = data;
+            }
+        }
+        int get(int i,int j){
+            if (i==j){
+                return this->array[i];
+            }
+            return 0;
+        }
+        void display(){
+            for (int i = 0; i < this->size; i++){
+                for (int j = 0; j < this->size; j++){
+                    if(i==j){
+                        cout<<this->array[j]<<" ";
+                        continue;
+                    }
+                    cout<<0<<" ";
+                }
+                cout<<endl;
+            }
+            
+        }
+};
+int main(int argc, char const *argv[]){
+    Matrix* matrix = new Matrix(4);
+
+    matrix->set(5,0,0);
+    matrix->set(6,1,1);
+    matrix->set(8,2,2);
+    matrix->set(9,3,3);
+    matrix->display();
+
+    cout<<matrix->get(1,2)<<endl;
+    return 0;
+}
